290e83243b6aa1833d7c34e0b2a275f6a5a08c86
gong <403575226@qq.com>
2018-07-02 20:56:31 +0800

    往中心服注册消息

--- src/main/java/cc/mi/record/server/RecordServerManager.java
+++ src/main/java/cc/mi/record/server/RecordServerManager.java
@@ -1,10 +1,26 @@
 package cc.mi.record.server;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import cc.mi.core.constance.IdentityConst;
+import cc.mi.core.handler.Handler;
 import cc.mi.core.manager.ServerManager;
 
 public class RecordServerManager extends ServerManager {
 	private static RecordServerManager instance;
+	
+	// 消息收到以后的回调
+	private static final Map<Integer, Handler> handlers = new HashMap<>();
+	private static final List<Integer> opcodes;
+	
+	static {
+		
+		opcodes = new LinkedList<>();
+		opcodes.addAll(handlers.keySet());
+	}
 	
 	public static RecordServerManager getInstance() {
 		if (instance == null) {
@@ -14,6 +30,6 @@ public class RecordServerManager extends ServerManager {
 	}
 	
 	public RecordServerManager() {
-		super(IdentityConst.SERVER_TYPE_RECORD);
+		super(IdentityConst.SERVER_TYPE_RECORD, opcodes);
 	}
 }
