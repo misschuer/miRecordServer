005cd4c27dc269dc38fc056db46736802240d13d
gong <403575226@qq.com>
2018-07-18 14:33:45 +0800

    增加日志服的逻辑

--- src/main/java/cc/mi/record/server/RecordServerManager.java
+++ src/main/java/cc/mi/record/server/RecordServerManager.java
@@ -4,17 +4,32 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import cc.mi.core.constance.IdentityConst;
 import cc.mi.core.handler.Handler;
+import cc.mi.core.log.CustomLogger;
 import cc.mi.core.manager.ServerManager;
+import cc.mi.core.packet.Packet;
 
 public class RecordServerManager extends ServerManager {
+	static final CustomLogger logger = CustomLogger.getLogger(RecordServerManager.class);
+	
 	private static RecordServerManager instance;
 	
 	// 消息收到以后的回调
 	private static final Map<Integer, Handler> handlers = new HashMap<>();
 	private static final List<Integer> opcodes;
+	
+	// 帧刷新
+	private static final ScheduledExecutorService excutor = Executors.newScheduledThreadPool(1);
+	// 消息包队列
+	private static final Queue<Packet> packetQueue = new LinkedList<>();
+	// 最后一次执行帧刷新的时间戳
+	protected long timestamp = 0;
 	
 	static {
 		
@@ -31,5 +46,52 @@ public class RecordServerManager extends ServerManager {
 	
 	public RecordServerManager() {
 		super(IdentityConst.SERVER_TYPE_RECORD, opcodes);
+		excutor.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				long prev = instance.timestamp;
+				long now = System.currentTimeMillis();
+				int diff = 0;
+				if (prev > 0) diff = (int) (now - prev);
+				instance.timestamp = now;
+				if (diff < 0 || diff > 1000) {
+					logger.warnLog("too heavy logical that execute");
+				}
+				try {
+					instance.doWork(diff);
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+		}, 1000, 100, TimeUnit.MILLISECONDS);
+	}
+	
+	/**
+	 * 进行帧刷新
+	 */
+	private void doWork(int diff) {
+		// 处理包信息
+		this.dealPacket();
+	}
+	
+	private void dealPacket() {
+		while (!packetQueue.isEmpty()) {
+			Packet packet = packetQueue.poll();
+			this.invokeHandler(packet);
+		}
+	}
+	
+	private void invokeHandler(Packet packet) {
+		int opcode = packet.getOpcode();
+		Handler handle = handlers.get(opcode);
+		if (handle != null) {
+			handle.handle(null, this.centerChannel, packet);
+		}
+	}
+	
+	public void pushPacket(Packet packet) {
+		synchronized (this) {
+			packetQueue.add(packet);
+		}
 	}
 }
