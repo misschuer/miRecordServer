ecfaf8f60031bf09a3c02a8d8e0efd478b16d753
xcsj <xcsj@192.168.0.108>
2018-12-20 10:46:21 +0800

    修改逻辑

--- src/main/java/cc/mi/record/Startup.java
+++ src/main/java/cc/mi/record/Startup.java
@@ -1,5 +1,8 @@
 package cc.mi.record;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import cc.mi.core.log.CustomLogger;
 import cc.mi.core.net.ClientCore;
 import cc.mi.record.config.ServerConfig;
@@ -10,20 +13,32 @@ public class Startup {
 	private static void start() throws NumberFormatException, Exception {
 		ServerConfig.loadConfig();
 		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
+		connectCenter();
+	}
+	
+	private static void connectCenter() {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(
+			new Runnable() {
+				@Override
+				public void run() {
 					while (true) {
-						ClientCore.INSTANCE.start(ServerConfig.getCenterIp(), ServerConfig.getCenterPort(), new RecordHandler());
-						logger.devLog("连接中心服错误,系统将在1秒钟后重新连接");
-						Thread.sleep(1000);
+						try {
+							ClientCore.INSTANCE.start(ServerConfig.getCenterIp(), ServerConfig.getCenterPort(), new RecordHandler());
+						} catch (Exception e) {
+							logger.errorLog(e.getMessage());
+						} finally {
+							logger.errorLog("连接中心服错误,系统将在1秒钟后重新连接");
+							try {
+								Thread.sleep(1000);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
-		}, "bootstrap-to-center").start();
+		);
 	}
 
 	public static void main(String[] args) throws NumberFormatException, Exception {
